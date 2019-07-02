84b6dd2160308615079e8e97634d97c14e58f1d4
Zero <john.royer@gmail.com>
2019-07-02 17:19:07 +0800

    Add code sum caculator

--- src/Ean13.php
+++ src/Ean13.php
@@ -17,4 +17,16 @@ class Ean13
     {
         return (13 == strlen($code));
     }
+
+    private static function getCheckSum($code)
+    {
+        $sum = 0;
+        for ($i = 0; $i < 13; $i++) {
+            if (0 == $i % 2) {
+                $sum += $code[$i] * 1;
+            } else {
+                $sum += $code[$i] * 3;
+            }
+        }
+    }
 }
