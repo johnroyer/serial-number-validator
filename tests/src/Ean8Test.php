b323dd87156713d631f953eea79825febceef296
Zero <john.royer@gmail.com>
2019-07-19 12:36:09 +0800

    fixup! Add test case prototype

--- tests/src/Ean8Test.php
+++ tests/src/Ean8Test.php
@@ -3,8 +3,17 @@
 namespace Zeroplex\Test;
 
 use PHPUnit\Framework\TestCase;
-use Zeroplex\Ean13;
+use Zeroplex\Ean8;
 
 class Ean8Test extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
 }
