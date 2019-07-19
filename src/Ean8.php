85cab830f096cde08777cb40c9190e2c50925a8c
Zero <john.royer@gmail.com>
2019-07-19 10:57:32 +0800

    Add prototype

---
+++ src/Ean8.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zeroplex;
+
+class Ean13
+{
+    public static function isValidate(string $code)
+    {
+    }
+
+    public static function isAllNumber($code)
+    {
+    }
+
+    public static function isLengthCorrect(string $code)
+    {
+    }
+
+    public static function getCheckSum($code)
+    {
+    }
+}
