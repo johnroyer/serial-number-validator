c08ddcda3816fdb131797413f87cfe0116747a90
Zero <john.royer@gmail.com>
2019-07-21 15:02:44 +0800

    Implement methods

--- src/Isbn13.php
+++ src/Isbn13.php
@@ -10,10 +10,12 @@ class Isbn13
 
     public static function is978Exist(string $code)
     {
+        return 1 === preg_match('/^978/', $code);
     }
 
     public static function isAllNumber(string $code)
     {
+        return 1 === preg_match('/^[0-9]+$/', $code);
     }
 
     public static function isLengthCorrect($code)
