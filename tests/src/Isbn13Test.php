e3f85d6303117aff2a37f29395fba4ad69b78f39
Zero <john.royer@gmail.com>
2019-07-21 15:02:28 +0800

    Add test case

--- tests/src/Isbn13Test.php
+++ tests/src/Isbn13Test.php
@@ -16,4 +16,55 @@ class Isbn13Test extends TestCase
     {
         parent::tearDown();
     }
+
+    /**
+     * @dataProvider code978Provider
+     */
+    public function test978Checker($in, $exp)
+    {
+        $this->assertSame(
+            $exp,
+            Isbn13::is978Exist($in)
+        );
+    }
+
+    public function code978Provider()
+    {
+        return [
+            ['9789862623893', true],
+            ['9789578787995', true],
+            ['9789576581557', true],
+            ['9789862357613', true],
+            ['9783836571135', true],
+            ['9781640092402', true],
+
+            ['2764171200420', false],
+            ['2764171160427', false],
+            ['2764171180425', false],
+        ];
+    }
+
+    /**
+     * @dataProvider codeProvider
+     */
+    public function testIfAllNumber($in, $exp)
+    {
+        $this->assertSame(
+            $exp,
+            Isbn13::isAllNumber($in)
+        );
+    }
+
+    public function codeProvider()
+    {
+        return [
+            ['7297465', true],
+            ['758067385', true],
+            ['12675843', true],
+
+            ['frergh', false],
+            ['498gd932', false],
+            ['iuh;fr*wfr', false],
+        ];
+    }
 }
