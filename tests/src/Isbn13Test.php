cb9d587032e6c534c54f942582da792f8b56463d
Zero <john.royer@gmail.com>
2019-07-21 15:01:36 +0800

    Add test case prototype

---
+++ tests/src/Isbn13Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zeroplex\Test;
+
+use PHPUnit\Framework\TestCase;
+use Zeroplex\Isbn13;
+
+class Isbn13Test extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+}
