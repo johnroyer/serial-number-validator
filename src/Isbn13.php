1e629aa7959c6952c4625ee28e34c054e40f42a1
Zero <john.royer@gmail.com>
2019-07-21 15:00:59 +0800

    Add ISBN checker prototype

---
+++ src/Isbn13.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Zeroplex;
+
+class Isbn13
+{
+    public static function isValidate(string $code)
+    {
+    }
+
+    public static function is978Exist(string $code)
+    {
+    }
+
+    public static function isAllNumber(string $code)
+    {
+    }
+
+    public static function isLengthCorrect($code)
+    {
+    }
+
+    public static function getCheckSum($code)
+    {
+    }
+}
