77d1ae664f448401c5d75d81440337c099ba27ea
Zero <john.royer@gmail.com>
2019-08-11 19:27:06 +0800

    Add prototype

--- src/TaiwanId.php
+++ src/TaiwanId.php
@@ -4,7 +4,19 @@ namespace Zeroplex;
 
 class TaiwanID
 {
-    public function isValid(string code)
+    public function isValid(string $code)
+    {
+    }
+
+    public static function islengthValid(string $code)
+    {
+    }
+
+    public static isGeoValid(string $code)
+    {
+    }
+
+    public static getCheckSum(string $code)
     {
     }
 }
